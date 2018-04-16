a735cf6589901f700ad1a4477df25f0e3531065d
Peter Goshulak <pgoshulak@gmail.com>
2018-04-16 15:13:35 -0400

    Clean up comments and formatting

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,13 @@
 var request = require('request');
 var fs = require('fs');
 var path = require('path');
-var GITHUB_TOKEN = require('./secrets').GITHUB_TOKEN
+var GITHUB_TOKEN = require('./secrets').GITHUB_TOKEN;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// Given repo owner and name, get all contributors
 function getRepoContributors(repoOwner, repoName, cb) {
+  // GET request options
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -14,15 +16,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+  // Send the request
   request(options, function(err, res, body) {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else {
       cb(err, body);
     }
   });
 }
 
+// Download an image from URL and save to filepath
 function downloadImageByURL(url, filePath) {
   var options = {
     url: url,
@@ -30,44 +34,45 @@ function downloadImageByURL(url, filePath) {
       'User-Agent': 'request',
       'Authorization': 'token ' + GITHUB_TOKEN
     }
-  }
+  };
   // Check that the write directory exists
   var dirname = path.dirname(filePath);
   if (!fs.existsSync(dirname)) {
-    fs.mkdirSync(dirname)
+    fs.mkdirSync(dirname);
   }
 
   // Request the file and pipe to stream
   request(options)
     .on('error', function(err) {
-      console.log(err)
+      console.log(err);
     })
     .on('response', function(res) {
-      console.log(`Received reponse "${res.statusCode}: ${res.statusMessage}" from ${url}`)
+      console.log(`Received reponse "${res.statusCode}: ${res.statusMessage}" from ${url}`);
     })
     .pipe(fs.createWriteStream(filePath)
       .on('finish', function() {
-        console.log(`Finished writing to ${filePath}`)
-      }))
+        console.log(`Finished writing to ${filePath}`);
+      }));
 }
 
+// Start of main program execution
 // Check required arguments
-var args = process.argv.slice(2)
+var args = process.argv.slice(2);
 
 if (args.length !== 2) {
-  console.log('Error: incorrect arguments supplied. Please provide 2 arguments as <user> <repo>, eg:')
-  console.log('  $ node download_avatars.js jquery jquery')
+  console.log('Error: incorrect arguments supplied. Please provide 2 arguments as <user> <repo>, eg:');
+  console.log('  $ node download_avatars.js jquery jquery');
 } else {
+  // Download list of all repo contributors
   getRepoContributors(args[0], args[1], function(err, result) {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else {
-      resultObj = JSON.parse(result)
+      resultObj = JSON.parse(result);
+      // Loop through each result obj and download the associated avatar URL
       resultObj.forEach(function(res) {
-        downloadImageByURL(res.avatar_url, `avatars/${res.login}.jpg`)
-      })
+        downloadImageByURL(res.avatar_url, `avatars/${res.login}.jpg`);
+      });
     }
   });
 }
-
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg")
