de0fb93e41d4c53f7f520872012f70568dabe1fb
Peter Goshulak <pgoshulak@gmail.com>
2018-04-16 13:29:00 -0400

    Implement request using github token

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,20 @@
 var request = require('request');
+var GITHUB_TOKEN = require('./secrets').GITHUB_TOKEN
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
