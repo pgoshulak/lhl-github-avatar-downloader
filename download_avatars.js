8a8d3596339df6fbe5313742b7a4cce98ef73324
Peter Goshulak <pgoshulak@gmail.com>
2018-04-16 14:36:53 -0400

    Add func to download single image to local dir

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,6 @@
 var request = require('request');
+var fs = require('fs');
+var path = require('path');
 var GITHUB_TOKEN = require('./secrets').GITHUB_TOKEN
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -21,7 +23,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+function downloadImageByURL(url, filePath) {
+  var options = {
+    url: url,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + GITHUB_TOKEN
+    }
+  }
+  // Check that the write directory exists
+  var dirname = path.dirname(filePath);
+  if (!fs.existsSync(dirname)) {
+    fs.mkdirSync(dirname)
+  }
+
+  // Request the file and pipe to stream
+  request(options)
+    .on('error', function(err) {
+      console.log(err)
+    })
+    .on('response', function(res) {
+      console.log(`Received reponse "${res.statusCode}: ${res.statusMessage}" from ${url}`)
+    })
+    .pipe(fs.createWriteStream(filePath)
+      .on('finish', function() {
+        console.log(`Finished writing to ${filePath}`)
+      }))
+}
+
+/* getRepoContributors("jquery", "jquery", function(err, result) {
   if (err) {
     console.log(err)
   } else {
@@ -30,4 +60,6 @@ getRepoContributors("jquery", "jquery", function(err, result) {
       console.log(`Avatar url :: ${res.avatar_url}`)
     })
   }
-});
+}); */
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg")
